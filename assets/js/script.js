e3ab965b733818cfdcd3f69cff32b30beaed5866
a-rod85 <31aaronanderson@gmail.com>
2022-07-05 08:27:13 +0000

    add to css body, genre-colum, add const & function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+const game = document.getElementById('the quiz')
+const scoreDisplay = document.getElementById ('score')
+
+const sport = 10
+const levels = ['medium', 'easy', 'easy' ]
+
+function addGenre()
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
 console.log("Connected!")
