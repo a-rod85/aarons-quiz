1f34515f1854f4e7c4ec1e3e9ef43b2be72d1833
a-rod85 <31aaronanderson@gmail.com>
2022-07-05 20:49:57 +0000

    add .card to css, add function and fetch to js, add card to fucntion on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
-const game-area = document.getElementById('the quiz')
+const game = document.getElementById('game')
 const scoreDisplay = document.getElementById ('score')
 
-const sport = 6
-const levels = ['medium', 'easy', 'hard']
+const sport = 21
+const levels = ['easy', 'medium', 'hard']
 
 function addGenre() {
     const column = document.createElement('div')
-    column.classList.add('game-area')
+    column.classList.add('genre-column')
     column.innerHTML = 'the genres'
-    game.append(column)
+    game.append(column);
 
     levels.forEach(level => {
-        fetch(`https://opentdb.com/api.php?amount=6&category=21&difficulty={level}&type=multiple`)
+
+        document.createElement('div')
+       card.classList.add('card')
+
+
+        fetch(`https://opentdb.com/api.php?amount=1&category=21&difficulty=${level}&type=boolean`)
         .then (response => response.json())
         .then (data => console.log(data))
     })
-    
 
 }
+
 
 addGenre()
  
 
 
-console.log("Connected!")
