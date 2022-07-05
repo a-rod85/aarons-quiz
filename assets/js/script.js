314f84c56fc26fe7e9df6fbe0aecce2501ce5943
a-rod85 <31aaronanderson@gmail.com>
2022-07-05 08:58:41 +0000

    add level.foreach in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
-const game = document.getElementById('the quiz')
+const game-area = document.getElementById('the quiz')
 const scoreDisplay = document.getElementById ('score')
 
-const sport = 10
-const levels = ['medium', 'easy', 'easy' ]
+const sport = 6
+const levels = ['medium', 'easy', 'hard']
 
-function addGenre()
+function addGenre() {
     const column = document.createElement('div')
-    column.classList.add('genre-column')
+    column.classList.add('game-area')
+    column.innerHTML = 'the genres'
+    game.append(column)
+
+    levels.forEach(level => {
+        fetch(`https://opentdb.com/api.php?amount=6&category=21&difficulty={level}&type=multiple`)
+        .then (response => response.json())
+        .then (data => console.log(data))
+    })
+    
+
+}
+
+addGenre()
+ 
+
+
 console.log("Connected!")
