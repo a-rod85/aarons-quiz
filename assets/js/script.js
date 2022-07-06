41ed1e170dd245d72579b178ce958ff85b823d07
a-rod85 <31aaronanderson@gmail.com>
2022-07-06 12:19:46 +0000

    add const titles to js, if in levels.foreach add func for card turn add to css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,57 @@
-const game = document.getElementById('game')
+const game = document.getElementById('the quiz')
 const scoreDisplay = document.getElementById ('score')
 
-const sport = 21
+const titles = [
+    {   name: 'Sport'
+        id: 21
+
+    }
+]
 const levels = ['easy', 'medium', 'hard']
 
-function addGenre() {
+function addGenre(titles) {
     const column = document.createElement('div')
     column.classList.add('genre-column')
-    column.innerHTML = 'the genres'
-    game.append(column);
+    column.innerHTML = titles.name
+    
 
-    levels.forEach(level => {
+    
+levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card);
 
-       const card = document.createElement('div')
-       card.classList.add('card')
-       column.append (card)
+        if (level === 'easy') {
+            card.innerHTML = 25
 
+        }
 
-    fetch(`https://opentdb.com/api.php?amount=1&category=21&difficulty=${level}&type=boolean`)
-        .then (response => response.json())
-        .then (data => { 
-            console.log(data)
-            card.setAttribute('data-question',data.results[1].question )    
-        
-        })
+        if  (level === 'medium') {
+            card.innerHTML = 50
+        }
+
+        if  (level === 'hard') {
+            card.innerHTML = 100
+        }
+
+        fetch(`https://opentdb.com/api.php?amount=1&category=${titles.id}&type=boolhttps://opentdb.com/api.php?amount=1&category=21&difficulty=${level}&type=booleann`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                card.setAttribute('data-question', data.results[0].question)
+                card.setAttribute('data-answer', data.results[0].correct_answer)
+            
+            })
+           card.addEventListener('click', card-turns)
     })
+        
+}
 
+function cardturns() {
+    console.log('clicked')
 }
 
 
-addGenre()
- 
+addGenre(titles[0])
 
 
