f97b2f3c2a6cff9be20eee6da3b551b72702b766
a-rod85 <31aaronanderson@gmail.com>
2022-07-08 13:50:59 +0000

    add new color to css, add submit btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,53 +3,49 @@ const scoreDisplay = document.getElementById('score');
 let score = 0
 let questionScore = 0
 
-const titles = ['Books', 'Film', 'Music', 'Video Games'];
+const genres = [
+    { name: 'Books', id: 10 },
+    { name: 'Film', id: 11 },
+    { name: 'Music', id: 12 },
+    { name: 'Video Games', id: 15 }
+]
 
-
-const levels = ['easy', 'medium', 'hard'];
+const levels = [
+    { name: 'easy', id: 100 },
+    { name: 'medium', id: 200 },
+    { name: 'hard', id: 300 }
+]
 
 function addGenre() {
-    titles.forEach(title => {
+    genres.forEach(genre => {
         const column = document.createElement('div');
         column.classList.add('genre-column');
-        
-        column.innerHTML = title;
-        if (title === 'Books') {
-            category = 10;
-        }
-        if (title === 'Film') {
-            category = 11;
-        }
-        if (title === 'Music') {
-            category = 12;
-        }
-        if (title === 'Video Games') {
-            category = 15;
-        }
+
+        const columnTitle = document.createElement('div');
+        columnTitle.classList.add('genre-title');
+        columnTitle.innerHTML = genre.name;
+
+        column.append(columnTitle)
+
         levels.forEach(level => {
+
             const card = document.createElement('div');
             card.classList.add('card');
-            column.append(card);
-
-            if (level === 'easy') {
-                card.innerHTML = 25;
-            }
-
-            if (level === 'medium') {
-                card.innerHTML = 50;
-            }
-
-            if (level === 'hard') {
-                card.innerHTML = 100;
-            }
-            fetch(`https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${level}&type=boolean`)
+            card.setAttribute('value', level.id)
+            fetch(`https://opentdb.com/api.php?amount=1&category=${genre.id}&difficulty=${level.name}&type=boolean`)
                 .then(response => response.json())
                 .then(data => {
-                    
                     card.setAttribute('data-question', data.results[0].question);
                     card.setAttribute('data-answer', data.results[0].correct_answer);
                 })
+
+            const cardValue = document.createElement('div')
+            cardValue.classList.add('value')
+            cardValue.innerHTML = card.getAttribute('value')
+
+            card.append(cardValue)
             card.addEventListener('click', cardturns, { once: true })
+            column.append(card);
         })
         quiz.append(column);
     })
@@ -58,41 +54,49 @@ function addGenre() {
 function cardturns() {
     console.log('clicked');
 
-    questionScore = this.innerText;
+    questionScore = this.getAttribute('value')
 
     const textDisplay = document.createElement('div');
-    const trueButton = document.createElement('button');
-    const falseButton = document.createElement('button');
-
-    falseButton.innerHTML = 'False';
-    trueButton.innerHTML = 'True';
-
-    falseButton.addEventListener('click', getResult)
-    trueButton.addEventListener('click', getResult)
-
+    textDisplay.classList.add('question');
     textDisplay.innerHTML = this.getAttribute('data-question')
 
-    this.append(textDisplay, trueButton, falseButton);
-    textDisplay.innerHTML = this.getAttribute('data-question');
-    const allCards = Array.from(document.querySelectorAll('.card'));
+    const buttonDiv = document.createElement('div')
+    buttonDiv.classList.add('buttons')
 
-    
+    const trueButton = document.createElement('button');
+    trueButton.classList.add('true')
+    trueButton.innerHTML = 'True';
+    trueButton.addEventListener('click', getResult)
+
+    const falseButton = document.createElement('button');
+    falseButton.classList.add('false')
+    falseButton.innerHTML = 'False';
+    falseButton.addEventListener('click', getResult)
+
+    buttonDiv.append(trueButton, falseButton)
+
+    this.append(textDisplay, buttonDiv);
 }
 
 function getResult() {
-    const cardOfButton = this.parentElement
+    const cardOfButton = this.parentElement.parentElement
+    console.log(this.parentElement)
+    console.log(cardOfButton.firstChild)
+
     if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
-        console.log('yes another one!')
+
         score = score + parseInt(questionScore)
         scoreDisplay.innerHTML = score
+
         cardOfButton.classList.add('correct-answer')
         setTimeout(() => {
             while (cardOfButton.firstChild) {
                 cardOfButton.removeChild(cardOfButton.lastChild)
             }
-            
-            cardOfButton.innerHTML = questionScore;
-            cardOfButton.style.color = '#000'
+            const resultValue = document.createElement('div')
+            resultValue.classList.add('value')
+            resultValue.innerHTML = questionScore
+            cardOfButton.append(resultValue)
         }, 40)
 
     } else {
@@ -101,18 +105,11 @@ function getResult() {
             while (cardOfButton.firstChild) {
                 cardOfButton.removeChild(cardOfButton.lastChild)
             }
-            cardOfButton.innerHTML = 0
+            const resultValue = document.createElement('div')
+            resultValue.classList.add('value')
+            resultValue.innerHTML = 0
+            cardOfButton.append(resultValue)
         }, 40)
     }
 }
 addGenre()
-
-const username = document.getElementById('login')
-const submitbutton = document.getElementById('submit')
- 
-var name = ''
-submitbutton.onclick = function()
-{console.log(username)
-document.getElementById('nameholder').innerText= username.value
-
-}
